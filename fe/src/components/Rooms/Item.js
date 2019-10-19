822751cf2b573452af50aff18262e547b9b83a5e
tranchien2002 <chie.tm2002@gmail.com>
2019-10-19 15:27:57 +0700

    connect iot fe

--- fe/src/components/Rooms/Item.js
+++ fe/src/components/Rooms/Item.js
@@ -1,35 +1,54 @@
-import React from 'react';
+import React, { Component } from 'react';
+import { compose } from 'redux';
+import { connect } from 'react-redux';
+import store from 'store';
+import * as actions from 'actions';
 
-const Item = props => {
-  const door = props.status
-    ? require('assets/img/close.jpg')
-    : require('assets/img/open.jpg');
-  return (
-    <div className='col-lg-3 col-md-3 col-xs-12 mb-2 col-'>
-      <div className='card'>
-        <img
-          className='card-img-top'
-          src={door}
-          alt='Foo eating a sandwich.'
-          style={{ height: '200px' }}
-        />
-        <div className='card-body'>
-          <h5 className='card-title'>{props.item}</h5>
-          <p className='card-text'>
-            {!props.status ? 'Door is opening' : 'Door is closing'}
-          </p>
-          <div className='row'>
-            <div className='col-12'>
-              {props.status ? (
-                <button className='btn btn-success'>Open</button>
-              ) : (
-                <button className='btn btn-dark'>Close</button>
-              )}
+class Item extends Component {
+  constructor(props) {
+    super(props);
+  }
+
+  async changeLock() {
+    await this.props.item.instance.methods.changeLock().send({ from: this.props.tomo.account });
+  }
+
+  render() {
+    const door = this.props.item.lock
+      ? require('assets/img/close.jpg')
+      : require('assets/img/open.jpg');
+    return (
+      <div className='col-lg-3 col-md-3 col-xs-12 mb-2'>
+        <div className='card'>
+          <img
+            className='card-img-top'
+            src={door}
+            alt='Foo eating a sandwich.'
+            style={{ height: '200px' }}
+          />
+          <div className='card-body'>
+            <h5 className='card-title'>room</h5>
+            <p className='card-text'>
+              {!this.props.item.lock ? 'Door is opening' : 'Door is closing'}
+            </p>
+            <div className='row'>
+              <div className='col-12'>
+                {this.props.item.lock ? (
+                  <button className='btn btn-success'>Open</button>
+                ) : (
+                  <button className='btn btn-dark'>Close</button>
+                )}
+              </div>
             </div>
           </div>
         </div>
       </div>
-    </div>
-  );
+    );
+  }
+}
+const mapStateToProps = (state) => {
+  return {
+    tomo: state.tomo
+  };
 };
-export default Item;
+export default compose(connect(mapStateToProps))(Item);
